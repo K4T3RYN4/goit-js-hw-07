1b64d028c85c86a6afd7ebffd9e3bd1fb8fc1238
kate <kateryna1morozova@gmail.com>
2025-08-12 19:58:56 +0300

    4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginFormEl = document.querySelector('.login-form');
+
+loginFormEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(loginFormEl);
+    formData.get('email');
+    formData.get('password');
+    const userInfo = Object.fromEntries(formData);
+
+
+    if (userInfo.email.trim() === '' || userInfo.password.trim() === '') {
+        alert('All form fields must be filled in');
+    } else {
+        console.log(userInfo);
+        loginFormEl.reset();
+    }
+}
+);
 
