ab12a9d68bc108391b29663d74f3a489297b3ceb
kate <kateryna1morozova@gmail.com>
2025-08-12 20:17:48 +0300

    5 finish

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const colorBtn = document.querySelector('.change-color');
+const docBody = document.querySelector('body');
+const colorName = document.querySelector('.color');
+
+colorBtn.addEventListener('click', colorBtnClickHandler);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+function colorBtnClickHandler() {
+  docBody.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = docBody.style.backgroundColor;
+};
